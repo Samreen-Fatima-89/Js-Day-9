dce9b89722a5dadd89637d735e011952ffe01cab
Samreen Fatima <irfankhans786787789@gmail.com>
2024-09-19 21:35:16 +0500

    completed

---
+++ app.js
@@ -0,0 +1,19 @@
+// var students = ["Ahmed", "Zain", "Faizan"];
+
+// for (var i = 0; i > students.length; i = i + 1) {
+//   console.log(students[i]);
+//   if (students[i] === "Zain") {
+//     console.log("--- Chal Gya ---");
+//   }
+// }
+
+// var ahmed = "Ahmed-Khan-Niazi";
+// var firstArray = [1, 51, 51, 5, 5, 1, 65];
+// console.log(firstArray.join(""));
+
+// var students = ["Ahmed", "Ali", "Muhammad"];
+// console.log(students.reverse().join(""));
+
+var alphabets = ["A", "C", "D", "X", "H"];
+alphabets.sort();
+console.log(alphabets);
